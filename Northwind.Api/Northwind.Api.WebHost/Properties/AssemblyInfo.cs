406aa0ab72fe498fe0c0c8f644d7b5e4c059dfb8
Patrick Ineichen <github@frokonet.ch>
2016-02-04 00:08:20 +0100

    Implemented Northwind.Api.WebHost

--- Northwind.Api/Northwind.Api.WebHost/Properties/AssemblyInfo.cs
+++ Northwind.Api/Northwind.Api.WebHost/Properties/AssemblyInfo.cs
@@ -1,5 +1,9 @@
 ﻿using System.Reflection;
 
+using Microsoft.Owin;
+
+using Northwind.Api;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -7,3 +11,5 @@
 [assembly: AssemblyCompany("frokonet.ch")]
 [assembly: AssemblyProduct("Northwind.Api")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
+
+[assembly: OwinStartup(typeof(Startup))]
